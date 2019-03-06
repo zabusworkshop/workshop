5f2c12d41e60b53c010075415ee7acd0f459f74d
ezabus <eugeny.zabus@sap.com>
2019-03-06 12:07:18 +0300

    basic event handling

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("tinyapp.controller.Main", {
 
         onInit: function () {
 
+        },
+        
+        onListSelectionChange: function (oEvent) {
+        	MessageToast.show("Welcome to " + oEvent.getParameter("listItem").getTitle());
         }
     });
 });
