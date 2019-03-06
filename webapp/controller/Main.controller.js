5419114e92fba19731d6f0cc7867b948e4c165ac
ezabus <eugeny.zabus@sap.com>
2019-03-06 13:37:10 +0300

    describe basic data binding

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,17 +1,29 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "tinyapp/utils/welcome"
-], function (Controller, welcome) {
+    "tinyapp/utils/welcome",
+    "sap/ui/model/json/JSONMOdel"
+], function (Controller, welcome, JSONModel) {
     "use strict";
 
     return Controller.extend("tinyapp.controller.Main", {
 
         onInit: function () {
-
+        	this.getView().setModel(new JSONModel({
+        		cities: [{
+        			id: 1,
+        			name: "Moscow"
+        		}, {
+        			id: 2,
+        			name: "St. Petersburg"
+        		}]
+        	}), "viewModel");
         },
         
         onListSelectionChange: function (oEvent) {
-        	welcome.greet(oEvent.getParameter("listItem").getTitle());
+        	var oBindingContext = oEvent.getParameter("listItem").getBindingContext("viewModel");
+        	var sTitle = oBindingContext.getProperty("name");
+        	welcome.greet(sTitle);
+        	this.getView().byId("title").setBindingContext(oBindingContext, "viewModel");
         }
     });
 });
